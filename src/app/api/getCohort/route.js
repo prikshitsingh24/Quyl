c86318e4eafae110830fa77ea08e7814dafc647c
Prikshit singh <prikshitsingh79@gmail.com>
2024-12-24 20:55:09 +0530

    Fix prisma findMany syntax error

--- src/app/api/getCohort/route.js
+++ src/app/api/getCohort/route.js
@@ -5,7 +5,7 @@ import { prisma } from "../prisma";
 
 export async function GET(req){
     try{
-        const cohort = await prisma.cohort.findMany();
+        const cohort = await prisma.cohort.findMany({});
         return NextResponse.json({cohort},{status:200})
     }catch(error){
         return NextResponse.json({error},{status:500});
