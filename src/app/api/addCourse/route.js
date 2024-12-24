ca9d6f5895ddd95932e40c2075a23184c53cd42b
Prikshit singh <prikshitsingh79@gmail.com>
2024-12-24 20:17:41 +0530

    Add courseName field to addCourse

--- src/app/api/addCourse/route.js
+++ src/app/api/addCourse/route.js
@@ -2,13 +2,14 @@ import { NextResponse } from "next/server";
 import { prisma } from "../prisma";
 
 export async function POST(req) {
-    const { cohortId } = await req.json();
+    const { cohortId,courseName } = await req.json();
 
     try {
       // Create a new course and link it to the cohort
       const course = await prisma.course.create({
         data: {
           cohortId,    // Foreign key to the cohort
+          courseName
         },
       });
       return NextResponse.json({course});  // Send the newly created course data as a response
